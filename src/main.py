bce1375bd26b5925e36d34f0ffab9f44339c3010
Karthik68771 <91474612+Karthik68771@users.noreply.github.com>
2022-01-03 19:44:27 +0530

    Update main.json

--- src/main.py
+++ src/main.py
@@ -60,7 +60,7 @@ def get_bot_data(app, message):
     message.continue_propagation()
 
 
-@app.on_message(Filters.command(['stats', 'stats@inhumanDexBot']))
+@app.on_message(Filters.command(['stats', 'stats@PikaDexBot']))
 def get_stats(app, message):
     if message.from_user.id in Config.sudo:
         members = 0
@@ -78,7 +78,7 @@ def get_stats(app, message):
 
 
 # ===== Home =====
-@app.on_message(Filters.command(['start', 'start@inhumanDexBot']))
+@app.on_message(Filters.command(['start', 'start@PikaDexBot']))
 def start(app, message):
     app.send_message(
         chat_id=message.chat.id,
@@ -87,7 +87,7 @@ def start(app, message):
     )
 
 # ==== Type Pokemon =====
-@app.on_message(Filters.command(['type', 'type@inhumanDexBot']))
+@app.on_message(Filters.command(['type', 'type@PikaDexBot']))
 def ptype(app, message):
     try:
         gtype = message.text.split(' ')[1]
@@ -153,7 +153,7 @@ def ptype_buttons(user_id):
         InlineKeyboardButton('Delete',callback_data=f"hexa_delete_{user_id}")]])
     return keyboard
     
-@app.on_message(Filters.command(['types', 'types@inhumanDexBot']))
+@app.on_message(Filters.command(['types', 'types@PikaDexBot']))
 def types(app, message): 
     user_id = message.from_user.id
     app.send_message(
@@ -217,7 +217,7 @@ def button2(client: app, callback_query: CallbackQuery):
         )
   
 # ===== Pokemon Type Command ======
-@app.on_message(Filters.command(['ptype', 'ptype@inhumanDexBot']))
+@app.on_message(Filters.command(['ptype', 'ptype@PikaDexBot']))
 def poketypes(app, message): 
     user_id = message.from_user.id
     try:
@@ -314,10 +314,10 @@ def poketypes_back(client: app, callback_query: CallbackQuery):
         
 # ===== Data command =====
 @app.on_callback_query(Filters.create(lambda _, query: 'basic_infos' in query.data))
-@app.on_message(Filters.command(['data', 'data@inhumanDexBot']))
+@app.on_message(Filters.command(['data', 'data@PikaDexBot']))
 def pkmn_search(app, message):
     try:
-        if message.text == '/data' or message.text == '/data@inhumanDexBot':
+        if message.text == '/data' or message.text == '/data@PikaDexBot':
             app.send_message(message.chat.id, texts['error1'], parse_mode='HTML')
             return None
         pkmn = func.find_name(message.text)
@@ -473,7 +473,7 @@ def locations(app, call):
 
 # ===== Usage command =====
 @app.on_callback_query(Filters.create(lambda _, query: 'usage' in query.data))
-@app.on_message(Filters.command(['usage', 'usage@inhumanDexBot']))
+@app.on_message(Filters.command(['usage', 'usage@PikaDexBot']))
 def usage(app, message):
     try:
         page = int(re.split('/', message.data)[1])
@@ -501,7 +501,7 @@ def usage(app, message):
 
 
 # ===== FAQ command =====
-@app.on_message(Filters.command(['faq', 'faq@inhumanDexBot']))
+@app.on_message(Filters.command(['faq', 'faq@PikaDexBot']))
 def faq(app, message):
     text = texts['faq']
     app.send_message(
@@ -514,7 +514,7 @@ def faq(app, message):
 
 
 # ===== About command =====
-@app.on_message(Filters.command(['about', 'about@inhumanDexBot']))
+@app.on_message(Filters.command(['about', 'about@PikaDexBot']))
 def about(app, message):
     text = texts['about']
     markup = InlineKeyboardMarkup([[
@@ -533,15 +533,15 @@ def about(app, message):
 
 
 # ===== Raid commands =====
-@app.on_message(Filters.command(['addcode', 'addcode@inhumanDexBot']))
+@app.on_message(Filters.command(['addcode', 'addcode@PikaDexBot']))
 def call_add_fc(app, message):
     raid.add_fc(app, message, texts)
 
-@app.on_message(Filters.command(['mycode', 'mycode@inhumanDexBot']))
+@app.on_message(Filters.command(['mycode', 'mycode@PikaDexBot']))
 def call_show_my_fc(app, message):
     raid.show_my_fc(app, message, texts)
 
-@app.on_message(Filters.command(['newraid', 'newraid@inhumanDexBot']))
+@app.on_message(Filters.command(['newraid', 'newraid@PikaDexBot']))
 def call_new_raid(app, message):
     raid.new_raid(app, message, texts)
 
